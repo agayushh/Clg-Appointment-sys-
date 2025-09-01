f3023d1032a1ff2ee35f7d1f507acfe6a7a96642
agayushh <agayush088@gmail.com>
2025-09-01 22:28:45 +0530

    feat: Add JWT authentication with access token generation for user registration and login; include type definitions for jsonwebtoken and ms

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,22 @@
-import { User } from "../models/user.model";
+import mongoose, { mongo } from "mongoose";
+import { Iuser, User } from "../models/user.model";
 import { asyncHandler } from "../utils/asyncHandler";
+
+const generateAccessToken = async (userId: mongoose.Types.ObjectId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user?.generateAccessToken();
+    return accessToken;
+  } catch (error) {
+    throw new Error("Error generating access token");
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password, role } = req.body;
+  if ([username, email, password, role].some((field) => field?.trim() === "")) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
   //check if the role is specified and valid
   const validRoles = ["student", "professor"];
@@ -22,7 +36,6 @@ const registerUser = asyncHandler(async (req, res) => {
   if (existingUser) {
     return res.status(400).json({ message: "User already exists" });
   }
-
   // Create new user
   const newUser = new User({
     username,
@@ -32,14 +45,26 @@ const registerUser = asyncHandler(async (req, res) => {
     ...(role === "professor" && { profId: req.body.profId }),
   });
   await newUser.save();
-  const registeredUser = await User.findOne({ email }).select("-password");
+  const accessToken = await generateAccessToken(
+    newUser._id as mongoose.Types.ObjectId
+  );
 
-  res
-    .status(201)
-    .json({ message: "User registered successfully", user: registeredUser });
+  const { password: _, ...userWithoutPassword } = newUser.toObject();
+
+  const option = {
+    httpOnly: true,
+    secure: true,
+  };
+  res.status(201).cookie("accessToken", accessToken, option).json({
+    message: "User registered successfully",
+    user: userWithoutPassword,
+  });
 });
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "email and password required" });
+  }
 
   // Find user by email
   const user = await User.findOne({ email });
@@ -52,9 +77,21 @@ const loginUser = asyncHandler(async (req, res) => {
   if (!isPasswordValid) {
     return res.status(400).json({ message: "Invalid email or password" });
   }
-  const loggedInUser = await User.findById(user._id).select("-password");
 
-  res.status(200).json({ message: "Login successful", user: loggedInUser });
+  const accessToken = await generateAccessToken(
+    user._id as mongoose.Types.ObjectId
+  );
+
+  const { password: _, ...userWithoutPassword } = user.toObject();
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .json({ message: "Login successful", user: userWithoutPassword });
 });
 
 export { registerUser, loginUser };
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 enum Roles {
   Student = "student",
@@ -12,6 +13,7 @@ export interface Iuser extends mongoose.Document {
   role: Roles;
   profId?: string;
   validatePassword(password: string): Promise<boolean>;
+  generateAccessToken(): string;
 }
 
 const userSchema = new mongoose.Schema<Iuser>(
@@ -56,6 +58,18 @@ userSchema.methods.validatePassword = async function (
   password: string
 ): Promise<boolean> {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      role: this.role,
+    },
+    process.env.SECRET_KEY!,
+    { expiresIn: "1h" }
+  );
 };
 
 export const User = mongoose.model<Iuser>("User", userSchema);
